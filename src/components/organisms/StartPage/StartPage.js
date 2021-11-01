e0ecd51af70c709ab3d8792363c47113eb80f941
sebast4an <szczypinskisebastian@gmail.com>
2021-11-01 18:49:47 +0100

    final main page and routing

--- src/components/organisms/StartPage/StartPage.js
+++ src/components/organisms/StartPage/StartPage.js
@@ -2,11 +2,23 @@ import { StyledList, StyledListElement } from 'components/atoms/StyledList/Style
 import { StyledParagraph } from 'components/atoms/StyledParagraph/StyledParagraph';
 import { Title } from 'components/atoms/Title/Title';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  display: block;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSize.s};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  text-decoration: none;
+  margin: 30px 0;
+`;
 
 const StartPage = () => (
   <>
     <Title>Hi! 👋</Title>
     <StyledParagraph>It is Game build in React, all words are fetch from API.</StyledParagraph>
+    <StyledLink to="/game">Start Game!</StyledLink>
     <Title>Rules: </Title>
     <StyledParagraph>You have 10 tries</StyledParagraph>
     <Title>Technologies used to build this game:</Title>
